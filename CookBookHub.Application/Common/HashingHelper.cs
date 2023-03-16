39e51e72354d8419ea41f04bedae12fde64703fd
Thanh Vu <thanhauco@gmail.com>
2023-03-16 09:48:00 -0700

    feat: Add hashing helper for security
    - Created HashingHelper with SHA256 hashing
    - Added GenerateRandomToken method
    - Security utilities for future authentication
    - Prepared for password hashing in v2.0

---
+++ CookBookHub.Application/Common/HashingHelper.cs
@@ -0,0 +1,29 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CookBookHub.Application.Common;
+
+public static class HashingHelper
+{
+    public static string ComputeSha256Hash(string rawData)
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+
+    public static string GenerateRandomToken(int length = 32)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var random = new Random();
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
