dc776df48578321f6a72b4b3f757cfc672d7b60d
Thanh Vu <thanhauco@gmail.com>
2023-03-21 14:08:00 -0700

    feat: Implement Unit of Work pattern
    - Created UnitOfWork class for transaction management
    - Added BeginTransaction, Commit, and Rollback methods
    - Implemented proper disposal pattern
    - Exception handling for transaction failures

---
+++ CookBookHub.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,66 @@
+using Microsoft.EntityFrameworkCore;
+using CookBookHub.Infrastructure.Data;
+
+namespace CookBookHub.Infrastructure.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly CookBookDbContext _context;
+    private Microsoft.EntityFrameworkCore.Storage.IDbContextTransaction? _transaction;
+
+    public UnitOfWork(CookBookDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        _transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+    }
+
+    public async Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await SaveChangesAsync(cancellationToken);
+            if (_transaction != null)
+            {
+                await _transaction.CommitAsync(cancellationToken);
+            }
+        }
+        catch
+        {
+            await RollbackTransactionAsync(cancellationToken);
+            throw;
+        }
+        finally
+        {
+            if (_transaction != null)
+            {
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
+        }
+    }
+
+    public async Task RollbackTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        if (_transaction != null)
+        {
+            await _transaction.RollbackAsync(cancellationToken);
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+    }
+
+    public void Dispose()
+    {
+        _transaction?.Dispose();
+        _context.Dispose();
+    }
+}
