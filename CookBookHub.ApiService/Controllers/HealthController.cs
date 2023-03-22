ed6726629953eec9a1b41c958c1559bba9c93028
Thanh Vu <thanhauco@gmail.com>
2023-03-22 10:53:00 -0700

    feat: Add health check endpoint
    - Created HealthController for monitoring
    - Returns status, timestamp, and version
    - Useful for load balancers and monitoring tools

---
+++ CookBookHub.ApiService/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookBookHub.ApiService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "Healthy",
+            timestamp = DateTime.UtcNow,
+            version = "1.0.0"
+        });
+    }
+}
