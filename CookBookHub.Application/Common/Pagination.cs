865dff18e7d5aac898d91f8c2257531187581cf9
Thanh Vu <thanhauco@gmail.com>
2023-03-31 19:53:00 -0700

    feat: Add pagination support
    - Created PagedResult class for paginated responses
    - Added PaginationParams for request parameters
    - Includes total pages, has previous/next page properties
    - Max page size limit of 50

---
+++ CookBookHub.Application/Common/Pagination.cs
@@ -0,0 +1,26 @@
+namespace CookBookHub.Application.Common;
+
+public class PagedResult<T>
+{
+    public IEnumerable<T> Items { get; set; } = new List<T>();
+    public int TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasPreviousPage => Page > 1;
+    public bool HasNextPage => Page < TotalPages;
+}
+
+public class PaginationParams
+{
+    private const int MaxPageSize = 50;
+    private int _pageSize = 10;
+
+    public int Page { get; set; } = 1;
+    
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+}
