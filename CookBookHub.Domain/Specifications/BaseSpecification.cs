be81b3ba419a0ab9293367c7d91da56b80e9e876
Thanh Vu <thanhauco@gmail.com>
2023-03-31 17:25:00 -0700

    feat: Add specification pattern for complex queries
    - Created ISpecification interface
    - Implemented BaseSpecification abstract class
    - Support for criteria, includes, and ordering
    - Enables reusable query logic

---
+++ CookBookHub.Domain/Specifications/BaseSpecification.cs
@@ -0,0 +1,39 @@
+using System.Linq.Expressions;
+
+namespace CookBookHub.Domain.Specifications;
+
+public interface ISpecification<T>
+{
+    Expression<Func<T, bool>> Criteria { get; }
+    List<Expression<Func<T, object>>> Includes { get; }
+    Expression<Func<T, object>>? OrderBy { get; }
+    Expression<Func<T, object>>? OrderByDescending { get; }
+}
+
+public abstract class BaseSpecification<T> : ISpecification<T>
+{
+    public Expression<Func<T, bool>> Criteria { get; private set; } = null!;
+    public List<Expression<Func<T, object>>> Includes { get; } = new();
+    public Expression<Func<T, object>>? OrderBy { get; private set; }
+    public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+    protected void AddInclude(Expression<Func<T, object>> includeExpression)
+    {
+        Includes.Add(includeExpression);
+    }
+
+    protected void ApplyOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+
+    protected void ApplyOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+    {
+        OrderByDescending = orderByDescExpression;
+    }
+
+    protected void ApplyCriteria(Expression<Func<T, bool>> criteria)
+    {
+        Criteria = criteria;
+    }
+}
